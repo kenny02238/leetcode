969b87a54a0d9b1105c49b69c52752c003bfe693
Kenny <kenny02238@yahoo.com.tw>
2023-11-29 16:48:06 +0800

    feat(js): add maximum depth of b tree js

---
+++ BTree/104.Maximum_Depth_of_BinaryTree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
+// link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+// 解題思路：
+// 1. 遞迴
+// 2. 每一層都加一，並且取左右子樹的最大值
